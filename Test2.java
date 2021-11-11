08f368fcbdc2df955ecc44bc1cad760c5165e853
AsAnt <93052826+AsiminaAnt@users.noreply.github.com>
2021-11-11 22:07:43 +0200

    Add file Test2
    Ηθελα να το δοκιμασω

---
+++ Test2.java
@@ -0,0 +1,13 @@
+public class Test2{
+
+	public static void main(String[] args){
+		int x=2;
+		double y=2.3;
+		boolean flag=true;
+
+		if(flag){
+			System.out.println("I wanted to add a file..nothing more..");
+		}
+	}
+
+}
